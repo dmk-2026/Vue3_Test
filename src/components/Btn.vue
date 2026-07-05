3659c00c176ed97e27453c069b999d18e50e76c9
dmk-2026 <2094806757@qq.com>
2026-07-05 22:25:42 +0800

    Initial commit

---
+++ src/components/Btn.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    type: String,
+    default: '按钮'
+  }
+})
+
+const emit = defineEmits(['change-text'])
+
+const handleClick = () => {
+  emit('change-text', '按钮点击了')
+}
+</script>
+<template>
+  <el-button @click="handleClick">{{ text }}</el-button>
+</template>
